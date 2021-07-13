d1919a48ebf375dea3b12f066e0e02efca026b4f
lvly <lavalikmr792@gmail.com>
2021-07-13 11:01:47 +0530

    google signin authentication

--- app/src/main/java/apps/develop/quizarea/MainActivity.java
+++ app/src/main/java/apps/develop/quizarea/MainActivity.java
@@ -9,57 +9,155 @@ import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends AppCompatActivity {
+
+   FirebaseAuth mAuth;
+   FirebaseUser mUser;
+    Button register;
+    private GoogleSignInClient mGoogleSignInClient;
+    private  static  final int RC_SIGN_IN=101;
+    ImageView googleLogin;
+private SignInButton signInButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        signInButton=findViewById(R.id.signButton);
+  /*      createRequest();*/
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mAuth = FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
-        ViewPager2 viewPager = findViewById(R.id.viewPager);
+      signInButton.setOnClickListener(new View.OnClickListener() {
 
-        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager(),getLifecycle());
-        pagerAdapter.addFragmet(new LoginFragment());
-        pagerAdapter.addFragmet(new RegisterFragment());
-        viewPager.setAdapter(pagerAdapter);
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
 
-    class AuthenticationPagerAdapter extends FragmentStateAdapter {
+    /*private void createRequest() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }*/
+    private  void  signIn(){
+        Intent signInIntent=mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        public AuthenticationPagerAdapter(@NonNull  FragmentManager fragmentManager,  Lifecycle lifecycle) {
-            super(fragmentManager, lifecycle);
-        }
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
 
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-
-            switch (position)
-            {
-                case 1:
-                    return new RegisterFragment();
-
+            if (requestCode == RC_SIGN_IN) {
+                // The Task returned from this call is always completed, no need to attach
+                // a listener.
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
             }
-
-            return new LoginFragment();
-
         }
 
-        void addFragmet(Fragment fragment) {
-            fragmentList.add(fragment);
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+            // Signed in successfully, show authenticated UI.
+
+            Toast.makeText(this,"Sign success", Toast.LENGTH_SHORT).show();
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        @Override
-        public int getItemCount() {
-            return fragmentList.size();
+    }
+
+    private void firebaseAuthWithGoogle( String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Toast.makeText(MainActivity.this,"Sign successfull", Toast.LENGTH_SHORT).show();
+                            updateUI(user);
+
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                           updateUI(null);
+
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Intent intent=new Intent(MainActivity.this,HomePage.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser user= mAuth.getCurrentUser();
+        if(user!=null){
+            Intent intent=new Intent(getApplicationContext(),HomePage.class);
+            startActivity(intent);
         }
     }
 
---
+++ app/src/main/java/apps/develop/quizarea/RegisterActivity.java
@@ -0,0 +1,60 @@
+package apps.develop.quizarea;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText et_name, et_email, et_password, et_repassword;
+    Button btn_register;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        et_name = findViewById(R.id.et_name);
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        et_repassword = findViewById(R.id.et_repassword);
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = et_name.getText().toString();
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                String repassword = et_repassword.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Register successful", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(RegisterActivity.this, "Register Unsuccessful", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+}
